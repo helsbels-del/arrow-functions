f92e6c34621bf310fa83a6b5a944b28f5e4b33a1
Helen Smith <5b64nnhi@students.codeinstitute.net>
2024-12-08 16:00:52 +0000

    FEAT: Add arrow functions

--- arrow-functions.js
+++ arrow-functions.js
@@ -4,11 +4,41 @@
  */
 
 // Vanilla JavaScript Function
+function addTwoNumbers(a, b) {
+    // Code block
+    return a + b;
+}
+let sum = addTwoNumbers(3, 5);
+console.log(sum);
 
 // Arrow Function With Parameters
+const add2TwoNumbers = (a, b) => {
+    // Code block
+    return a + b;
+}
+let any = add2TwoNumbers(3, 5);
+console.log(any);
 
 // Single Line Arrow Function With Parameters
+const addTwoNumbers2 = (a,b) => a + b;
+let sum2 = addTwoNumbers2(6, 4);
+console.log(sum2);
+
 
 // Implicit Returns
 
+const saySomething = message => console.log(message);
+saySomething('Hello there!!')
+
+const sayHello = () => console.log('hello');
+sayHello();
+
+
 // Returning Multiple Lines
+
+const returnMultipleines = () => (
+    `<p>
+    This is a multiline string!
+    </P>`
+)
+console.log(returnMultipleines());
